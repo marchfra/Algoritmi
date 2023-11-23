71773bc3797b21b34827b46b9f6b31d70390ed67
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2023-11-23 21:03:32 +0100

    Reformatted root_finders.cpp

---
+++ Libs/Docs/html/search/enums_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['error_0',['Error',['../class_vector.html#a244bab3c6312d26dec3dfcdfbaf3774f',1,'Vector']]]
+];
---
+++ Libs/Docs/html/search/functions_d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['vector_0',['vector',['../class_vector.html#a39d6069675db4ecfc1ab81d440da759a',1,'Vector::Vector()'],['../class_vector.html#abb2481f4fb5ba83925359ea7cff5f1c6',1,'Vector::Vector(const int &amp;size)'],['../class_vector.html#ad8f8b12edf86fb25f25e74590561d6fb',1,'Vector::Vector(const int &amp;size, T initialValue)'],['../class_vector.html#af1d5502e6e32c524493a9575df3662d0',1,'Vector::Vector(const std::string &amp;name)'],['../class_vector.html#a042ce8485f6bc348a6b39a289ff22916',1,'Vector::Vector(const std::string &amp;name, const int &amp;size)'],['../class_vector.html#a0f3e394bac711ad814c8b798d1667386',1,'Vector::Vector(const std::string &amp;name, const int &amp;size, T initialValue)'],['../class_vector.html#a7f20fa12a3d3658a93a16c1bf6d28810',1,'Vector::Vector(const Vector&lt; T &gt; &amp;V)']]],
+  ['vverlet_1',['vVerlet',['../ode__solver_8hpp.html#ad13872a4d07109fa920ebab81fdbb5e2',1,'ode_solver.cpp']]]
+];
