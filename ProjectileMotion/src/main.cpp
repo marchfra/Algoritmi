120e275fae546715b6f902ba63997164606cf746
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2024-05-05 12:04:20 +0200

    Computed residual of y(x). Problem in second shooting

--- ProjectileMotion/src/main.cpp
+++ ProjectileMotion/src/main.cpp
@@ -1,87 +1,266 @@
-#include <iostream>
+/**
+ * @file main.cpp
+ *
+ * @author Francesco Marchisotti
+ *
+ * @brief Main file for the final project
+ *
+ * @version 0.1
+ *
+ * @date 2024-05-04
+ */
+
+#include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/lin_alg.hpp"
+#include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/ode_solver.hpp"
+#include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/root_finder.hpp"
+
+#include <cmath>
 #include <fstream>
 #include <iomanip>
-#include <cmath>
+#include <iostream>
 
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/swap.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/quad.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/polynomials.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/root_finder.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/derivative.hpp"
-#include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/ode_solver.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/lin_alg.hpp"
-
-using std::cout;
-using std::cin;
 using std::cerr;
+using std::cin;
+using std::cout;
 using std::endl;
 
-const static double b = 1;
+// Problem data
+const static double B  = 4.0e-5;  //!< Drag coefficient
+const static double V0 = 10.0;    //!< Initial velocity
+const static double L  = 10.0;    //!< Target distance
+double gTheta;                    //!< Initial launch angle
 
+// Dimensional factors
+const static double chi = L;
+const static double mu  = 1.0;
+const static double g   = 9.81;
+const static double tau = sqrt(chi / g);
+const static double b   = B * chi / mu;
+const static double v0  = V0 * tau / chi;
+
+/**
+ * @brief Right Hand Side of the ODEs system
+ *
+ * @param[in] Y The input values of the variables
+ * @param[out] R The output values of the variables
+ */
 void RHS(double Y[], double R[]);
+
+/**
+ * @brief Residual function of x(t)
+ *
+ * This function takes in input a time value and integrates the system of ODEs
+ * upto (and including) that time, and returns the value of x(t) - 1 in order to
+ * find the time at which x == 1.
+ *
+ * @param[in] time The time at which to evaluate x
+ *
+ * @return x(t) - 1
+ */
+double x_t(const double& time);
+
+/**
+ * @brief Residual function of y(t)
+ *
+ * This function takes in input a time value and integrates the system of ODEs
+ * upto (and including) that time, and returns the value of y(t*) in order to
+ * find the x-position at which y == 0.
+ *
+ * @param[in] time The time at which to evaluate y
+ *
+ * @return y(t)
+ */
+double y_t(const double& time);
 
 int main() {
 	std::ofstream out;
-	out.open("data/data.csv");
+
+	// Constants output
+	out.open("data/constants.csv");
 	if (!out) exit(5);
 
-	// Integration range
-	double t = 0.0;
-	const double t_max = 10.0;
-	const double dt = 0.01;
-	const int nStep = (t_max - t) / dt;
-
-	// Initialisation
-	const double theta[] = {
-		40.0 / 180 * M_PI,
-		45.0 / 180 * M_PI,
-		50.0 / 180 * M_PI};
-	const double v0 = 1.0;
-	const double y0[] = {0.0, 0.0, v0 * cos(theta[0]), v0 * sin(theta[0])};
-	const double y1[] = {0.0, 0.0, v0 * cos(theta[1]), v0 * sin(theta[1])};
-	const double y2[] = {0.0, 0.0, v0 * cos(theta[2]), v0 * sin(theta[2])};
-	double y[4];
-	const int nEq = static_cast<int>(sizeof(y0)) / static_cast<int>(sizeof(y0[0]));
-
-	out << "t,x,y,u,v,theta,drag_coeff" << endl;
-
-	for (int i = 0; i < nEq; i++) y[i] = y0[i];
-	out << t << "," << y[0] << "," << y[1] << "," << y[2] << "," << y[3] << "," << theta[0] << "," << b << endl;
-	for (int i = 0; i < nStep; i++) {
-		pVerlet(t, y, RHS, dt, nEq);
-		t += dt;
-
-		out << t << "," << y[0] << "," << y[1] << "," << y[2] << "," << y[3] << "," << theta[0] << endl;
-
-		if (y[1] < 0.0) break;
-	}
-
-	t = 0.0;
-	for (int i = 0; i < nEq; i++) y[i] = y1[i];
-	out << t << "," << y[0] << "," << y[1] << "," << y[2] << "," << y[3] << "," << theta[1] << "," << b << endl;
-	for (int i = 0; i < nStep; i++) {
-		pVerlet(t, y, RHS, dt, nEq);
-		t += dt;
-
-		out << t << "," << y[0] << "," << y[1] << "," << y[2] << "," << y[3] << "," << theta[1] << "," << b << endl;
-
-		if (y[1] < 0.0) break;
-	}
-
-	t = 0.0;
-	for (int i = 0; i < nEq; i++) y[i] = y2[i];
-	out << t << "," << y[0] << "," << y[1] << "," << y[2] << "," << y[3] << "," << theta[2] << "," << b << endl;
-	for (int i = 0; i < nStep; i++) {
-		pVerlet(t, y, RHS, dt, nEq);
-		t += dt;
-
-		out << t << "," << y[0] << "," << y[1] << "," << y[2] << "," << y[3] << "," << theta[2] << endl;
-
-		if (y[1] < 0.0) break;
-	}
-
+	out << "chi,tau,mu,B,b" << endl;
+	out << chi << "," << tau << "," << mu << "," << B << "," << b << endl;
 
 	out.close();
+
+
+	// Integration range
+	const double t0    = 0.0;   // Starting point
+	const double t_max = 10.0;  // Ending point
+	const int nStep    = 1000;  // Number of steps
+	const double dt    = (t_max - t0) / nStep;
+
+	// Initialisation
+	double t          = t0;
+	const double y0[] = {0.0, 0.0, v0 * cos(gTheta), v0 * sin(gTheta)};
+	const int nEq =
+		static_cast<int>(sizeof(y0)) / static_cast<int>(sizeof(y0[0]));
+
+	// Initial angle
+	const double theta0    = 0.68;  // Initial guess for launch angle
+	const double theta_max = 0.9;   // Max value for theta
+	const int nTheta       = 32;    // Number of explored thetas
+	const double dTheta    = (theta_max - theta0) / nTheta;
+
+
+	/* +---------------------------------+
+	 * | STEP 1: INTEGRATING UP TO x = 1 |
+	 * +---------------------------------+ */
+
+	const double x_max  = 1.0;
+	const double x_over = 0.1;
+
+	// ------------ SHOOTING ------------
+	out.open("data/shooting.csv");
+	if (!out) exit(5);
+
+	out << "t,x,y,u,v,theta" << endl;  // Output file header
+	gTheta = theta0;
+	for (int j = 0; j < nTheta; j++) {
+		t          = t0;
+		double y[] = {0.0, 0.0, v0 * cos(gTheta),
+		              v0 * sin(gTheta)};  // Initialize solution
+		out << tau * t << "," << chi * y[0] << "," << chi * y[1] << ","
+			<< chi / tau * y[2] << "," << chi / tau * y[3] << "," << gTheta
+			<< endl;  // Output initial condition
+		// Integration
+		int counter = 0;
+		while (y[0] < x_max + x_over && counter < nStep) {
+			pVerlet(t, y, RHS, dt, nEq);
+			t += dt;
+
+			out << tau * t << "," << chi * y[0] << "," << chi * y[1] << ","
+				<< chi / tau * y[2] << "," << chi / tau * y[3] << "," << gTheta
+				<< endl;
+			counter++;
+		}
+		gTheta += dTheta;
+	}
+	out.close();
+
+
+	/* +-----------------------------------+
+	 * | STEP 2: FINDING ROOTS OF x(t) = 1 |		Maybe step 1 can be implemented inside x_t
+	 * +-----------------------------------+ */
+
+	const double t_low = 1.0;     //!< Lower bound for root searching
+	const double t_upp = 2.0;     //!< Upper bound for root searching
+	const double eps_t = 1.0e-7;  //!< Precision for root searching
+
+	out.open("data/x_t.csv");
+	if (!out) exit(5);
+
+	out << "t*,theta" << endl;
+	gTheta = theta0;
+	double
+		t_star[64];  //!< Array with the times at which x == 1. Length == nTheta
+	for (int i = 0; i < nTheta; i++) {
+		double roots[4];
+		int nRoots;
+		try {
+			findRoots(x_t, t_low, t_upp, eps_t, roots, nRoots);
+		}
+		catch (std::runtime_error e) {
+			cerr << "You fucked up (step 2). " << e.what() << endl;
+		}
+
+		t_star[i] = roots[0];
+
+		out << tau * t_star[i] << "," << gTheta << endl;
+
+		gTheta += dTheta;
+	}
+	out.close();
+
+	// cout << "t* = ";
+	// printVector(t_star, nTheta);
+
+
+	/* +---------------------------------+
+	 * | STEP 3: RESIDUAL PLOT OF y(x=1) |
+	 * +---------------------------------+ */
+
+	// ------------ SHOOTING ------------
+	std::ofstream out2;
+	out2.open("data/shooting2.csv");
+	if (!out2) exit(5);
+
+	out2 << "t,x,y,u,v,theta" << endl;
+	out2.close();
+
+	out.open("data/residual.csv");
+	if (!out) exit(5);
+
+	out << "t*,y*,theta" << endl;  // Output file header
+	gTheta = theta0;
+	for (int j = 0; j < nTheta; j++) {
+		out << tau * t_star[j] << "," << chi * y_t(t_star[j]) << "," << gTheta
+			<< endl;
+		gTheta += dTheta;
+	}
+	out.close();
+
+
+	/* +---------------------------------+
+	 * | STEP 4: FINDING ROOTS OF y(x=1) |
+	 * +---------------------------------+ */
+
+	// const double theta_low = theta0;     //!< Lower bound for root searching
+	// const double theta_upp = theta_max;  //!< Upper bound for root searching
+	// const double eps_theta = 1.0e-7;     //!< Precision for root searching
+
+	// out.open("data/optimal.csv");
+	// if (!out) exit(5);
+
+	// out << "theta*" << endl;
+	// double theta_star[64];  //!< Array with the thetas at which y(x=1) = 0
+	// int nTheta_star;        //!< Length of theta_star
+	// try {
+	// 	// This can't work: y_t is a function of time alone, and I'm looking for
+	// 	// roots in theta.
+	// 	findRoots(y_t, theta_low, theta_upp, eps_theta, theta_star, nTheta_star, 2);
+	// }
+	// catch (std::runtime_error e) {
+	// 	cerr << "You fucked up (step 4). " << e.what() << endl;
+	// }
+	// for (int i = 0; i < nTheta_star; i++) {
+	// 	out << theta_star[i] << endl;
+	// }
+	// out.close();
+
+	// cout << "Optimal thetas: ";
+	// printVector(theta_star, nTheta_star);
+
+
+	/* +---------------------------------+
+	 * | STEP 5: INTEGRATING WITH THETA* |
+	 * +---------------------------------+ */
+
+	out.open("data/final_trajectories.csv");
+	if (!out) exit(5);
+
+	out << "t,x,y,u,v,theta" << endl;  // Output file header
+	gTheta     = 0.68905;
+	t          = t0;
+	double y[] = {0.0, 0.0, v0 * cos(gTheta),
+					v0 * sin(gTheta)};  // Initialize solution
+	out << tau * t << "," << chi * y[0] << "," << chi * y[1] << ","
+		<< chi / tau * y[2] << "," << chi / tau * y[3] << "," << gTheta
+		<< endl;  // Output initial condition
+	// Integration
+	int counter = 0;
+	while (y[0] < x_max && counter < nStep) {
+		pVerlet(t, y, RHS, dt, nEq);
+		t += dt;
+
+		out << tau * t << "," << chi * y[0] << "," << chi * y[1] << ","
+			<< chi / tau * y[2] << "," << chi / tau * y[3] << "," << gTheta
+			<< endl;
+		counter++;
+	}
+	out.close();
+
 	return 0;
 }
 
@@ -91,10 +270,63 @@ void RHS(double Y[], double R[]) {
 	double u = Y[2];
 	double v = Y[3];
 
-	double mod_v = sqrt(u*u + v*v);
+	double mod_v = sqrt(u * u + v * v);
 
 	R[0] = u;
 	R[1] = v;
 	R[2] = -b * u * mod_v;
-	R[3] = -b * 1.0 - u * mod_v;
+	R[3] = -1.0 - b * u * mod_v;
+}
+
+double x_t(const double& time) {
+	// Integration range
+	const double t0    = 0.0;
+	const double t_max = time;
+	const int nStep    = 1000;
+	const double dt    = (t_max - t0) / nStep;
+
+	// Initialisation
+	double t      = t0;
+	double y[]    = {0.0, 0.0, v0 * cos(gTheta), v0 * sin(gTheta)};
+	const int nEq =
+		static_cast<int>(sizeof(y)) / static_cast<int>(sizeof(y[0]));
+
+	// Integration
+	for (int i = 0; i < nStep; i++) {
+		pVerlet(t, y, RHS, dt, nEq);
+		t += dt;
+	}
+	return y[0] - 1.0;
+}
+
+double y_t(const double& time) {
+	std::ofstream out;
+	out.open("data/shooting2.csv", std::ios_base::app);
+	if (!out) exit(5);
+
+	// Integration range
+	const double t0    = 0.0;
+	const double t_max = time;
+	const int nStep    = 1000;
+	const double dt    = (t_max - t0) / nStep;
+
+	// Initialisation
+	double t   = t0;
+	double y[] = {0.0, 0.0, v0 * cos(gTheta), v0 * sin(gTheta)};
+	const int nEq =
+		static_cast<int>(sizeof(y)) / static_cast<int>(sizeof(y[0]));
+
+	// Integration
+	out << tau * t << "," << chi * y[0] << "," << chi * y[1] << ","
+		<< chi / tau * y[2] << "," << chi / tau * y[3] << "," << gTheta
+		<< endl;  // Output initial condition
+	for (int i = 0; i < nStep; i++) {
+		pVerlet(t, y, RHS, dt, nEq);
+		t += dt;
+
+		out << tau * t << "," << chi * y[0] << "," << chi * y[1] << ","
+			<< chi / tau * y[2] << "," << chi / tau * y[3] << "," << gTheta
+			<< endl;
+	}
+	return y[1];
 }
--- ProjectileMotion/src/plot.py
+++ ProjectileMotion/src/plot.py
@@ -5,13 +5,42 @@ import pandas as pd
 
 # Constants
 IMAGES_FOLDER = 'images'
+SAVE_IMAGES = False
+DOWNSAMPLE = 1
+DRAW_LEGENDS = False
 
 # Matplotlib configuration
 plt.style.use(['grid', 'science', 'notebook', 'mylegend'])
 colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
+
+# +---------------------------+
+# | STEP 0: GETTING CONSTANTS |
+# +---------------------------+
+
+df = pd.read_csv('data/constants.csv')
+L = float(df['chi'][0])
+tau = float(df['tau'][0])
+mu = float(df['mu'][0])
+B = float(df['B'][0])
+b = float(df['b'][0])
+
+print(f'+------------------------------+')
+print(f'|           CONSTANTS          |')
+print(f'+------------------------------+')
+print(f'|         L = {L:<7.1f} m        |')
+print(f'|       tau = {tau:<7.5f} s        |')
+print(f'|        mu = {mu:<7.2f} kg       |')
+print(f'|         B = {B:<7.1e} kg/m     |')
+print(f'+------------------------------+')
+
+
+# +---------------------------------+
+# | STEP 1: INTEGRATING UP TO x = L |
+# +---------------------------------+
+
 # Import data
-df = pd.read_csv('data/data.csv')
+df = pd.read_csv('data/shooting.csv')
 
 # Manipulate data
 dfgroup = df.groupby(by=df['theta'])
@@ -19,24 +48,126 @@ groups = dfgroup.groups.keys()
 
 # Create figure
 fig, ax = plt.subplots(1, 1)
-for i, g in enumerate(groups):
+for i, g in enumerate(list(groups)[::DOWNSAMPLE]):
 	x = dfgroup.get_group(g)['x'].to_numpy()
 	y = dfgroup.get_group(g)['y'].to_numpy()
-	ax.plot(x, y, color=colors[i], label=f'{g * 180 / np.pi:.0f}°')
-# x = dfgroup.get_group(1)['x'].to_numpy()
-# y = dfgroup.get_group(1)['y'].to_numpy()
-# ax.plot(x, y)
+	ax.plot(x, y, c=colors[i % len(colors)], label=f'{g:.2f} rad')
+ax.axvline(L, c='k', ls='--', alpha=.7)
 
+ax.set_title(rf'Shooting up to $x = {L} + \epsilon$, $B = {B}$')
+ax.set_xlabel(r'$x$ [m]')
+ax.set_ylabel(r'$y$ [m]')
 
-# ax.set_xscale('log')
-# ax.set_yscale('log')
-
-ax.set_title('')
-ax.set_xlabel('x [adim]')
-ax.set_ylabel('y [adim]')
-
-ax.legend(title='Initial angle')
+if DRAW_LEGENDS:
+	ax.legend(title='Initial angle', ncol=int(
+		len(groups) / DOWNSAMPLE / 4), fontsize=15)
 
 fig.tight_layout()
-# fig.savefig(f'{IMAGES_FOLDER}/figure_name.png', dpi=200)
+if SAVE_IMAGES:
+	fig.savefig(f'{IMAGES_FOLDER}/figure_name.png', dpi=200)
+
+
+# +-----------------------------------+
+# | STEP 2: FINDING ROOTS OF x(t) = L |
+# +-----------------------------------+ */
+
+# Import data
+t_star = pd.read_csv('data/x_t.csv')['t*'].to_numpy()
+
+
+# Create figure
+fig, ax = plt.subplots(1, 1)
+for i, (g, T) in enumerate(zip(list(groups)[::DOWNSAMPLE], t_star[::DOWNSAMPLE])):
+	t = dfgroup.get_group(g)['t'].to_numpy()
+	x = dfgroup.get_group(g)['x'].to_numpy()
+	ax.plot(t, x, c=colors[i % len(colors)], label=f'{g:.2f} rad')
+	ax.axvline(T, ymax=.87, c=colors[i % len(colors)], ls='--', alpha=.3)
+ax.axhline(L, c='k', ls='--', alpha=.7)
+
+ax.set_title(rf'Finding $t^*$ s.t. $x(t^*) = {L}$, $B = {B}$')
+ax.set_xlabel(r'$t$ [s]')
+ax.set_ylabel(r'$x$ [m]')
+
+if DRAW_LEGENDS:
+	ax.legend(title='Initial angle', ncol=int(
+		len(groups) / DOWNSAMPLE / 4), fontsize=15)
+
+fig.tight_layout()
+if SAVE_IMAGES:
+	fig.savefig(f'{IMAGES_FOLDER}/figure_name.png', dpi=200)
+
+
+# +--------------------------------+
+# | STEP 2.5: SHOOTING UP TO x = L |
+# +--------------------------------+
+
+# Import data
+df2 = pd.read_csv('data/shooting2.csv')
+
+# Manipulate data
+df2group = df2.groupby(by=df2['theta'])
+groups2 = df2group.groups.keys()
+
+# Create figure
+fig, ax = plt.subplots(1, 1)
+for i, g in enumerate(list(groups2)[::DOWNSAMPLE]):
+	x = df2group.get_group(g)['x'].to_numpy()
+	y = df2group.get_group(g)['y'].to_numpy()
+	ax.scatter(x, y, c=colors[i % len(colors)], label=f'{g:.2f} rad')
+ax.axvline(L, c='k', ls='--', alpha=.7)
+
+ax.set_title(rf'Shooting up to $x = {L}$, $B = {B}$')
+ax.set_xlabel(r'$x$ [m]')
+ax.set_ylabel(r'$y$ [m]')
+
+if DRAW_LEGENDS:
+	ax.legend(title='Initial angle', ncol=int(
+		len(groups2) / DOWNSAMPLE / 4), fontsize=15)
+
+fig.tight_layout()
+if SAVE_IMAGES:
+	fig.savefig(f'{IMAGES_FOLDER}/figure_name.png', dpi=200)
+
+
+# +----------------------------+
+# | STEP 3: RESIDUAL OF y(x=L) |
+# +----------------------------+ */
+
+# Import data
+df_res = pd.read_csv('data/residual.csv')
+
+# Create figure
+fig, ax = plt.subplots(1, 1)
+ax.plot(df_res['theta'], df_res['y*'])
+ax.axhline(0, c='k', ls='--', alpha=0.7)
+
+ax.set_title(rf'Residual plot of $y(x={L})$, $B = {B}$')
+ax.set_xlabel(r'$\theta$ [rad]')
+ax.set_ylabel(r'$y(x=1)$ [m]')
+
+fig.tight_layout()
+if SAVE_IMAGES:
+	fig.savefig(f'{IMAGES_FOLDER}/figure_name.png', dpi=200)
+
+# +---------------------------------+
+# | STEP 5: INTEGRATING WITH THETA* |
+# +---------------------------------+ */
+
+# Import data
+df = pd.read_csv('data/final_trajectories.csv')
+
+# Create figure
+fig, ax = plt.subplots(1, 1)
+ax.plot(df['x'], df['y'])
+ax.axvline(L, c='k', ls='--', alpha=0.7)
+
+ax.set_title(rf'Optimal trajectory, $B = {B}$')
+ax.set_xlabel(r'$x$ [m]')
+ax.set_ylabel(r'$y$ [m]')
+
+fig.tight_layout()
+if SAVE_IMAGES:
+	fig.savefig(f'{IMAGES_FOLDER}/figure_name.png', dpi=200)
+
+
 plt.show()
