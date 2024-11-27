f54c82237cc287e2303612f0b8e75c3836d64c50
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2024-11-27 18:43:59 +0100

    Renamed Anno_4 to Magistrale

--- Libs/tests/test_config.hpp
+++ Libs/tests/test_config.hpp
@@ -15,4 +15,4 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 
-#include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/doctest.h"
+#include "/Users/francescomarchisotti/Documents/Uni/Magistrale/Algoritmi/doctest.h"
