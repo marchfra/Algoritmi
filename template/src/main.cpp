8897189e5d8391a6e14769bfcadfe3f2b8ea8a1b
Francesco Marchisotti <francesco.marchisotti@gmail.com>
2025-09-17 18:35:09 +0200

    Modified lib path according to new name of parent folders

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -3,13 +3,13 @@
 #include <iomanip>
 #include <cmath>
 
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/swap.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/quad.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/polynomials.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/root_finder.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/derivative.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/ode_solver.hpp"
-// #include "/Users/francescomarchisotti/Documents/Uni/Anno_4/Algoritmi/Libs/include/lin_alg.hpp"
+// #include "../../Libs/include/swap.hpp"
+// #include "../../Libs/include/quad.hpp"
+// #include "../../Libs/include/polynomials.hpp"
+// #include "../../Libs/include/root_finder.hpp"
+// #include "../../Libs/include/derivative.hpp"
+// #include "../../Libs/include/ode_solver.hpp"
+// #include "../../Libs/include/lin_alg.hpp"
 
 using std::cout;
 using std::cin;
